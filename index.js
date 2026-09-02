bdef650be5341f596ab3ab66f10b87632d50b725
Atharva7652005 <khairnasair2005@gmail.com>
2026-09-02 11:27:08 +0530

    Update SSH username in deploy workflow and modify response message in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
     return res.json({
-        msg: "Hello from the AWS server its Running"
+        msg: "Hello from the AWS server its Running, Let test "
     })
 })
 
